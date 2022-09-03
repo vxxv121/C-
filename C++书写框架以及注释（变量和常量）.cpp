1ea934b9c61abfabe87c85fd5e3a61293fc8018d
Floating code <61642294+vxxv121@users.noreply.github.com>
2022-09-03 21:31:40 +0800

    Add files via upload

---
+++ C++书写框架以及注释（变量和常量）.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//using namespace std;
+//#define Day 7 //Day的值是7
+//
+///*常量的定义方式
+//1.#define 宏常量,一般再代码前端
+//2.const修饰的变量，一般使用在 语句中*/
+//int main() {
+//
+//	//Day = 14;//这句语句是错误，DAY是常量，无法修改修改会报错
+//    cout << "一周总共有：" << Day << "天" << endl;
+//	
+//	//const修饰的变量
+//	//int month = 12;
+//	//month = 23 //正常变量值是可修改的
+//	const int month = 12;
+//	//	month = 23;    增加了const修饰变量后是无法修改的,所以这个会报错
+//	cout << "一年总共有；" << month << "个月份" << endl;
+//
+//	
+//	system("pause");
+//		return 0;
+//
+//}
+///* {
+// 
+//	cout << "123" << endl;
+//	system("pause");  //这是假的
+//
+//	 
+//
+//		return 0;
+//
+//}*/

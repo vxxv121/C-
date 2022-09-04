629b8984c6cdd5adcf102557fd27ba1efb100dea
Floating code <61642294+vxxv121@users.noreply.github.com>
2022-09-04 21:19:06 +0800

    Add files via upload

---
+++ 数据类型-布尔类型.cpp
@@ -0,0 +1,20 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	//1,创建bool是类型
+	bool flag = true;//true代表真
+	cout << flag << endl;
+
+	flag = false;//false代表假
+	cout << flag << endl;
+    
+	//2，查看bool类型所占内存空间  
+	
+	cout << "所占内存空间：" <<sizeof(bool)<< endl;
+	
+	system("pause");
+	return 0;
+
+
+}
+*/
